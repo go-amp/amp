ea6070b9765fe5450c29694f63bbd5b7b5465d97
breedx <bri4n.r33d@gmail.com>
2014-03-03 22:20:26 +0000

    test

--- serialize.go
+++ serialize.go
@@ -82,7 +82,7 @@ func UnpackMaps(buffer *[]byte, readBytes int, incoming_handler chan *map[string
             prefix := int(binary.BigEndian.Uint16(prefixBytes))        
             log.Println("looping2")
             // indicates end of incoming message
-            if prefix == 0 { if i >= readBytes { log.Println("sending to incoming1"); incoming_handler <- &ret; 
+            if prefix == 0 { if i >= readBytes { log.Println("sending to incoming1");// incoming_handler <- &ret; 
                 return 0 } else { break } }                            
             // message overflow
             if i + prefix > readBytes { recycleMap(&ret); return message_start }
@@ -111,7 +111,7 @@ func UnpackMaps(buffer *[]byte, readBytes int, incoming_handler chan *map[string
         }                
         //log.Println("am i getting here?")
         log.Println("sending to incoming2")
-        incoming_handler <- &ret
+        //incoming_handler <- &ret
     }         
     return 0
 }
